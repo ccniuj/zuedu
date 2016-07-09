1d2ce6ad52c0df3e8c69cc47831ee68f07ddb1c9
Juin Chiu <davidjuin0519@gmail.com>
2016-07-10 01:12:58 +0800

    add products RUD

--- app/controllers/dashboard/products_controller.rb
+++ app/controllers/dashboard/products_controller.rb
@@ -1,11 +1,46 @@
 class Dashboard::ProductsController < DashboardController
+  before_action :set_product, only: [:edit, :update, :destroy]
+
   def index
     @products = Product.all
     render json: @products
   end
 
   def edit
-    @product = Product.find_by id: params[:id]
     render json: @product
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      render json: { message: '新增成功' }, status: :created
+    else
+      render json: { message: '新增失敗' }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @product.update product_params
+      render json: { message: '更新成功' }, status: :ok
+    else
+      render json: { message: '更新失敗' }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      render json: { message: '刪除成功' }, status: :ok
+    else
+      render json: { message: '刪除失敗' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def set_product
+      @product = Product.find params[:id]
+    end
+
+    def product_params
+      params.require(:products).permit(:name, :description, :price, :inventory)
+    end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,9 +3,4 @@ class ProductsController < ApplicationController
     @products = Product.all
     render json: @products
   end
-
-  def edit
-    @product = Product.find_by id: params[:id]
-    render json: @product
-  end
 end
