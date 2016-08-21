55d02eedd80513fe9dc1fcec8172affec82cb5a6
Juin Chiu <davidjuin0519@gmail.com>
2016-08-22 00:22:24 +0800

    config gmail setting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,8 +19,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => 'davidjuin0519@gmail.com',
-    :password             => 'xxx',
+    :user_name            => ENV['GMAIL_ACCOUNT'],
+    :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
