78289012bdbda9f954177cb6e7dea37ce5eebccb
daniel840829 <daniel840829@yahoo.com.tw>
2017-04-21 01:17:14 +0800

    add mailer layout cvs_info、atm_info

--- app/controllers/allpay_controller.rb
+++ app/controllers/allpay_controller.rb
@@ -18,19 +18,8 @@ class AllpayController < ApplicationController
   def callback
     transaction = Transaction.find_by!(trade_number: params[:MerchantTradeNo])
     transaction.update!(params: request.POST)
-    logger.info(transaction["RtnCode"])
-    if transaction["RtnCode"]=="1"
-      logger.info "PAY"
-      send_pay_success_email! transaction
-    elsif transaction["RtnCode"]=="10100073"
-      logger.info "CVSSS"
-      send_cvs_info_email! transaction
-    elsif transaction["RtnCode"]=="2"
-      logger.info "ATMM"
-      send_atm_info_email! transaction
-    else
-      logger.info "error"
-    end
+    
+    
     render text: :'1|OK'
   rescue ActiveRecord::RecordNotFound
     render text: :'0|transaction record not found'
@@ -42,10 +31,5 @@ class AllpayController < ApplicationController
 
   private
 
-  def send_pay_success_email! transaction
-    transaction.order.line_items.each do |applicant|
-      MemberMailer.payment_success(applicant).deliver_now#remember change it to the deliver_later
-    end
-  end
-
+  
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ActiveRecord::Base
   belongs_to :order
   after_initialize :generate_trade_number
   validate :check_trade_number, :check_mac_value, on: :update
-
+  after_update :check_pay
   def to_param
     trade_number
   end
@@ -19,5 +19,37 @@ private
 
   def check_mac_value
     errors.add(:params, 'wrong mac value') unless Allpay.client.verify_mac(params)
+  end
+  def check_pay
+    logger.info(parms["RtnCode"])
+    if params["RtnCode"]=="1"
+      logger.info "PAY"
+      send_pay_success_email! self
+    elsif params.["RtnCode"]=="10100073"
+      logger.info "CVSSS"
+      send_cvs_info_email! self
+    elsif params.["RtnCode"]=="2"
+      logger.info "ATMM"
+      send_atm_info_email! self
+    else
+      logger.info "error"
+    end
+  end
+  def send_pay_success_email! transaction
+    transaction.order.line_items.each do |applicant|
+      MemberMailer.payment_success(applicant).deliver_now#remember change it to the deliver_later
+    end
+  end
+  def send_atm_info_email! transaction
+    @params=transaction.params
+    @email=transaction.order.line_items.parent_email
+    logger.info "params #{@params} ,email #{@email}"
+    MemberMailer.atm_info(@params,@email).deliver_now#remember change it to the deliver_later
+  end
+  def send_cvs_info_email! transaction
+    @params=transaction.params
+    @email=transaction.order.line_items.parent_email
+    logger.info "params #{@params} ,email #{@email}"
+    MemberMailer.cvs_info(@params,@email).deliver_now #remember change it to the deliver_later
   end
 end
