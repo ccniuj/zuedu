328a68652b7d333c934a6d84851534ef76721698
daniel840829 <daniel840829@yahoo.com.tw>
2017-04-22 20:28:50 +0800

    #degug8 mailer cvs_info atm_info

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -41,13 +41,16 @@ private
     end
   end
   def send_atm_info_email
+    logger.info "#{self}"
     self.each do |transaction|
     MemberMailer.atm_info(transaction).deliver_now#remember change it to the deliver_later
     end
   end
   def send_cvs_info_email
+    logger.info "#{self}"
     self.each do |transaction|
     MemberMailer.cvs_info(transaction).deliver_now #remember change it to the deliver_later
     end
   end
+
 end
